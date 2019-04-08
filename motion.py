2cad2d53a9c22c2322472b44426548919a24267f
Lucas Carpenter <lcscrpntr@gmail.com>
2019-04-07 21:50:11 -0700

    meh

--- motion.py
+++ motion.py
@@ -1,5 +1,16 @@
 import json
 
+def yes_no_query():
+    result = input("y/n | yes/no")
+    result = result.lower()
+    if result == 'y' or result == 'yes':
+        return True
+    elif result == 'n' or result == 'no':
+        return False
+    else:
+        print("Please Enter yes or no")
+        return yes_no_query()
+
 
 def do_discussion():
     discussion = input("Do I hear any discussion? (y/n)")
@@ -14,25 +25,33 @@ def do_discussion():
         do_discussion()
 
 
-motion_text = input('Motion is: ')
+def do_motion(motion_text, motion_maker):
+    print(f"Motion is {motion_text}")
+    print("[confirm summary of motion is correct with motion maker and note taker]")
+    input("Press any key to continue...")
+    print("Do I have a second?")
+    seconded = yes_no_query()
+    if not seconded:
+        return {
+            "motion text": motion_text,
+            "motion maker": motion_maker,
+            "seconded": seconded
+        }
 
-seconded = input("Second?")
+    do_discussion()
 
-do_discussion()
+    # todo: add handling for motion to table
+    # todo: add handling for call to question
 
-# todo: add handling for motion to table
-# todo: add handling for call to question
+    yay = input("Those in favor: ")
+    nay = input("Those opposed: ")
+    abstain = input("Those abstaining: ")
 
-yay = input("Those in favor: ")
-nay = input("Those opposed: ")
-abstain = input("Those abstaining: ")
+    output_text = json.dumps({
+        "motion text": motion_text,
+        "motion maker": motion_maker,
+        "seconded": seconded,
+        "result": (yay, nay, abstain)
+    })
 
-output_text = json.dumps({
-    "motion text": motion_text,
-    "seconded": seconded,
-    "result": (yay, nay, abstain)
-})
-
-output_file = open(f"{motion_text}.json", 'a')
-
-output_file.write(output_text)
+    return output_text
